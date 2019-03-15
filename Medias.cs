098cb163bc096ec446c2c68f67459400e8f5f81d
dcl24was05 <dcl24was05@iMac-de-iMac24w5.local>
2019-03-15 10:26:36 -0600

    problema000

--- Medias.cs
+++ Medias.cs
@@ -5,27 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AutomatizarPruebasUnitarias {
-    
-    class Medias {
+  class Medias {
+    /**
+      * Calcula y regresa la media artimética
+      */
+    public static double mediaAritmetica(params int[] vals) { }
 
-        /**
-         * Calcula y regresa la media artimética
-         */
-        public static double mediaAritmetica(params int[] vals) { }
+    /**
+      * Calcula y regresa la raíz enésima = x^(1/n)
+      */
+    private static double raizEnesima(double x, int n) { }
 
-        /**
-         * Calcula y regresa la raíz enésima = x^(1/n)
-         */
-        private static double raizEnesima(double x, int n) { }
+    /**
+      *  Usa raizEnesima para calcular y regresar la media geométrica
+      */
+    public double mediaGeometrica(params int[] vals) { }
 
-        /**
-         *  Usa raizEnesima para calcular y regresar la media geométrica
-         */
-        public double mediaGeometrica(params int[] vals) { }
-
-        /**
-         * Este método no está implementado.
-         */
-        public static double mediaArmonica(params int[] vals) { }
-    }
+    /**
+      * Este método no está implementado.
+      */
+    public static double mediaArmonica(params int[] vals) { }
+  }
 }
