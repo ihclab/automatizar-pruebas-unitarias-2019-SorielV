3acbc9f6ae571b23d81cf26b9bff98a8f7868ca1
20136769 CLEMENTE SORIEL VALLEJO MEJIA <20136769@TELEMATICA.UCOL.MX>
2019-03-20 11:24:06 -0600

    Problema 2: Leer los casos de prueba

---
+++ utils.js
@@ -0,0 +1,28 @@
+/*
+  Format => id:method:args:expected => '0001:mediaAritmetica:2 4 8:4.6667'
+*/
+
+const fs = require('fs')
+const Path = require('path')
+const { promisify } = require('util')
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
+const isNullOrUndefined = el => el === null || el === undefined
+const isEmptyEl = el => isNullOrUndefined(el) || !el.trim()
+
+module.exports = {
+  isNullOrUndefined,
+  importData: async (
+    file,
+    line_separator = process.env.line_separator || '\r\n',
+    field_separator = process.env.field_separator || ':',
+    args_separator = process.env.args_separator || ' '
+  ) => {
+    return (await readFile(file, 'utf-8'))
+      .split(line_separator)
+      // .filter(line => !line || line.trim())
+      .reduce((data, line, lineNumber) => {}, [])
+  }
+}
